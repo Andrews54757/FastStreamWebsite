fe699f8fa9f43d508bec234043b170ccfb54a758
Andrew S <andrews54757@gmail.com>
2024-03-29 14:13:44 -0500

    Update

--- player/utils/EnvUtils.mjs
+++ player/utils/EnvUtils.mjs
@@ -22,7 +22,7 @@ export class EnvUtils {
   static getVersion() {
     // eslint-disable-next-line prefer-const
     let version = '1.0.0.web';
-version = '1.3.2';
+version = '1.3.3';
     return this.isExtension() ? chrome.runtime.getManifest().version : version;
   }
   static isIncognito() {
